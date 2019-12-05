f1135f909e5ca839b9263cda59f3a58c5fe9d2fe
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2019-12-05 03:23:24 -0600

    -Users endpoint is functioning.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 let db = require("./data/db");
-
 // This creates an instance our express server.
 const app = express();
 
@@ -8,19 +7,36 @@ const app = express();
 app.use(express.json());
 
 // This is where our routes are defined.
+
+// This route is for the / path
 app.get("/", (req, res) => {
   // This logs the request's ip on the express server
   console.log("ip:", req.ip);
 
   // This will send a json message to the request
-  res.json({ message: "Welcome to the api" });
+  res.json({ message: "API server is up and running" });
+});
+
+// This route is for /users route
+// This route should display all users in db.js
+app.get("/api/users", (req, res) => {
+  // console.log("ip:", req.ip, Date.now());
+  db.find()
+    .then(users => {
+      return res.status(200).json(users);
+    })
+    .catch(() => {
+      return res.status(404).json({
+        error: "Cannot retrieve user list"
+      });
+    });
 });
 
 // This is the port the server will run on
 const port = 8080;
 // This is the local host address
-const host = "127.0.0.1"
+const host = "127.0.0.1";
 
 app.listen(port, host, () => {
-	console.log(`Server running at http://${host}:${port}`)
-})
+  console.log(`Server running at http://${host}:${port}, better go catch it`);
+});
