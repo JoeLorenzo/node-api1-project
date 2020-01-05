9b9a9210202c21338efad2003de7cd3f6011a25e
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-04 21:43:41 -0600

    -Added express dependency -Boilerplate server is running

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-require 
-// implement your API here
+const express = require("express")
+const server = express()
+
+port = 5000
+host = "127.0.0.1"
+server.listen(port, ()=> console.log(`server running ${host}:${port}`))
