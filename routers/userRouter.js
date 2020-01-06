c2233a99e5ce5edc74145d446af18378494a2302
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-06 00:50:00 -0600

    -User by ID endpoint is functioning

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,6 +3,7 @@ const express = require("express")
 const db = require("../data/db.js")
 const router = express.Router()
 router.use(express.json())
+
 router.get("/api/users", (req,res) => { 
     console.log("------time of request-------")
     // this console logs the time of the request 
@@ -17,8 +18,26 @@ router.get("/api/users", (req,res) => {
         console.log(user)
         res.status(200).json(user)
     })
-    
 })
-
+router.get("/api/user/:id", (req, res) => {
+    const id = req.params.id
+    db.findById(id)
+    .then( user => {
+        if (user){
+            console.log("-----------user-------------")
+            // this console logs the user of the request 
+            console.log(user)
+            console.log("----------------------------")      
+            res.status(200).json(user)
+        }
+        else {
+            console.log("-----------user-------------")
+            // this console the 404 message
+            console.log(`user id ${id} not found` )
+            console.log("----------------------------")
+            res.status(404).json({errorMessage:"User not found"})
+            }
+    })
+})
 // this syntax exports the module
 module.exports = router
