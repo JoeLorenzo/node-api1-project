bda592f39acd34f69c4e1140208af7672d5255d1
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-05 23:23:22 -0600

    -Added the all users endpoint

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,23 @@
 // this imports express
 const express = require("express")
-
+const db = require("../data/db.js")
 const router = express.Router()
-
-router.get("/api/users", (req,res) =>{
-    res.send("hello from the users api")
+router.use(express.json())
+router.get("/api/users", (req,res) => { 
+    console.log("------time of request-------")
+    // this console logs the time of the request 
+    console.log(Date())
+    console.log("----------------------------")
+    console.log("-------url of request-------")
+    // this console logs the url of the request 
+    console.log(req.url)
+    console.log("----------------------------")
+    db.find()
+    .then(user => {
+        console.log(user)
+        res.status(200).json(user)
+    })
+    
 })
 
 // this syntax exports the module
